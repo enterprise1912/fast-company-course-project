5cd6780dcc2c4770d4161ea5648b9f52f3457709
MacBook Classic <you@example.com>
2024-02-19 21:02:04 -0600

    added URL modification and data transformation to adapt to FireBase

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,12 +1,39 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import config from "../config.json";
+import configFile from "../config.json";
 
 // if we use an HTTP-service only to connect the application to one resource - our server, on backend, and no other websites/resources/URIs/etc, then define baseURL as follows:
-axios.defaults.baseURL = config.apiEndPoint;
+axios.defaults.baseURL = configFile.apiEndPoint;
+
+axios.interceptors.request.use(
+    function (config) {
+        if (configFile.ifFireBase) {
+            const containSlash = /\/$/gi.test(config.url);
+            config.url =
+                (containSlash ? config.url.slice(0, -1) : config.url) + ".json";
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+function transformData(data) {
+    return data
+        ? Object.keys(data).map((key) => ({
+              ...data[key]
+          }))
+        : [];
+}
 
 axios.interceptors.response.use(
-    (response) => response,
+    (response) => {
+        if (configFile.ifFireBase) {
+            response.data = { content: transformData(response.data) };
+        }
+        return response;
+    },
     function (error) {
         const isExpectedError =
             error.response &&
